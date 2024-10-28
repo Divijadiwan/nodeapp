2aaa8bdc5075f71b265f2be68c8e8a7d9bb84259
Divijadiwan <79202293+Divijadiwan@users.noreply.github.com>
2024-10-28 18:42:02 -0400

    updating open page

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("hi")
 
 // Import the express library and assign it to a variable
 import express from 'express'
@@ -9,6 +8,9 @@ const app = express()
 // Set the port the application will be running on
 const port = process.env.PORT || 3001
 
+app.get('/', (req, res) => {
+    res.send("hello i am divija")
+  })
 // Set up a response for the root path of the application
 app.get('/movies', (req, res) => {
     const genreName = req.query.genre; // Access the country name
